83f1f2e66398faa7dc2195cdf25970bcc7d100e3
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2025-11-20 13:06:50 +0500

    Add middleware for Stripe webhook handling in server configuration

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+app.use("/api/stripe/webhook", express.raw({ type: "application/json" }));
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
