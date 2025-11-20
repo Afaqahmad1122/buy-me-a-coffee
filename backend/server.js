a1880aaec74df60bb0dd5dd98a4224e9db1523df
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2025-11-20 13:20:56 +0500

    Refactor JSON middleware to conditionally bypass for Stripe webhook requests

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,12 @@ const PORT = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use("/api/stripe/webhook", express.raw({ type: "application/json" }));
-app.use(express.json());
+app.use((req, res, next) => {
+  if (req.originalUrl === "/api/stripe/webhook") {
+    return next();
+  }
+  express.json()(req, res, next);
+});
 app.use(helmet());
 app.use(morgan("dev"));
 
