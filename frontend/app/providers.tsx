18d08f95b62c3c5d7710fee749062000ea228528
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2025-11-20 15:27:42 +0500

    Add React Query and Axios dependencies, refactor layout to include ReactQueryProvider, and simplify Home component

---
+++ frontend/app/providers.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { PropsWithChildren, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export function ReactQueryProvider({ children }: PropsWithChildren) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            staleTime: 1000 * 30,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
